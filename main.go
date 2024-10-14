6163c4d8a043796d73068bab84894741c26e2fa0
arthurscarpin <scarpinarthur.dev@gmail.com>
2024-10-14 20:51:44 -0300

    feat: adicione metodo de depositar

--- main.go
+++ main.go
@@ -14,17 +14,25 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	if podeSacar {
 		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
 	}
+	return "Saldo insuficiente"
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "O depósito foi realizado com sucesso", c.saldo
+	}
+	return "Valor do depósito menor que zero", c.saldo
 }
 
 func main() {
 	contaDaSilvia := ContaCorrente{}
 	contaDaSilvia.titular = "Silvia"
 	contaDaSilvia.saldo = 500
-	
+
 	fmt.Println(contaDaSilvia.saldo)
-	fmt.Println(contaDaSilvia.Sacar(400))
-	fmt.Println(contaDaSilvia.saldo)
+	// fmt.Println(contaDaSilvia.Sacar(400))
+	status, valor := contaDaSilvia.Depositar(400)
+	fmt.Println(status, valor)
 }
