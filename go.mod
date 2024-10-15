d47279ee5b3eb02d25fec17bbb73e03d8fe1aa02
arthurscarpin <scarpinarthur.dev@gmail.com>
2024-10-14 21:49:38 -0300

    refactor: organiza o projeto em modulos

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gestao-de-contas-bancarias
+
+go 1.23.2
--- main.go
+++ main.go
@@ -1,47 +1,17 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	}
-	return "Saldo insuficiente"
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "O depósito foi realizado com sucesso", c.saldo
-	}
-	return "Valor do depósito menor que zero", c.saldo
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	}
-	return false
-}
+import (
+	"fmt"
+	"gestao-de-contas-bancarias/clientes"
+	"gestao-de-contas-bancarias/contas"
+)
 
 func main() {
-	contaDaSilvia := ContaCorrente{titular: "Silvia", saldo: 300}
-	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 100}
+	contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
+		Nome:      "Bruno",
+		CPF:       "123.111.123.12",
+		Profissao: "Desenvolvedor"},
+		NumeroAgencia: 123, NumeroConta: 123456, Saldo: 100}
 
-	status := contaDaSilvia.Transferir(-200, &contaDoGustavo)
-
-	fmt.Println(status)
-	fmt.Println(contaDaSilvia)
-	fmt.Println(contaDoGustavo)
+	fmt.Println(contaDoBruno)
 }
