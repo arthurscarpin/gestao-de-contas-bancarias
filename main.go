86b7bddeaf200a4b04f553ecbf932828a46fa932
arthurscarpin <scarpinarthur.dev@gmail.com>
2024-10-15 21:52:06 -0300

    feat: adiciona função para pagar um boleto e interface para verificar a conta

--- main.go
+++ main.go
@@ -5,9 +5,22 @@ import (
 	"gestao-de-contas-bancarias/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
-	contaDoDenis := contas.ContaPoupanca {}
+	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
-	contaDoDenis.Sacar(555)
+	PagarBoleto(&contaDoDenis, 60)
 	fmt.Println(contaDoDenis.ObterSaldo())
+
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 400)
+	fmt.Println(contaDaLuisa.ObterSaldo())
 }
